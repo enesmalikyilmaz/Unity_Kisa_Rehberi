1d5af6edab305c42b41fa0e8009796d1d54148d6
enesmalikyilmaz <121015121+enesmalikyilmaz@users.noreply.github.com>
2024-10-30 19:43:14 +0300

    Unity Değişiklikleri
    Unity projesi dosyaları güncellendi

---
+++ Assets/Scripts/Enemy_sc.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy_sc : MonoBehaviour
+{
+    [SerializeField]
+    public float speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        if(transform.position.y <-5.4f){
+            float randomX = Random.Range(-9.4f,+9.4f);
+
+            transform.position = new Vector3(randomX,7.5f,transform.position.z);
+        }
+    }
+    void OnTriggerEnter(Collider other){
+
+    if(other.tag == "Player"){
+
+        Player_sc player_sc = other.transform.GetComponent<Player_sc>();
+
+        if(player_sc != null){
+            player_sc.Damage();
+        }
+
+        Destroy(this.gameObject);
+        }
+     else if(other.tag == "Laser"){
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+    }
+    }
+
+   
+}
---
+++ Assets/Scripts/Player_sc.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_sc : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject laserPrefab; 
+    public Transform firePoint;    
+    public float fireRate = 0.5f;  // Seri atışlar arası süre
+    private float nextFire = 0f;   // Bir sonraki atış zamanı
+    public float speed = 1f;
+    public float lives = 3.0f;
+    SpawnManager_sc spawnManager_sc;
+
+    void Start(){
+        spawnManager_sc = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager_sc>();
+        if(spawnManager_sc== null){
+            Debug.Log("Spawn_Manager oyun nesnesi bulunamadı");
+        }    }
+
+    void Update()
+    {
+        CalculateMovement();
+
+        // Space tuşuna basılı tutulduğunda ve bekleme süresi dolduğunda ateş et
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            FireLaser();
+        }
+    }
+    void FireLaser()
+    {
+        Instantiate(laserPrefab, transform.position, Quaternion.identity);
+    }
+
+    void CalculateMovement(){
+
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        transform.position += new Vector3(moveHorizontal,moveVertical,0) * speed * Time.deltaTime;
+
+        if(transform.position.y<-3.9f){
+             transform.position = new Vector3(transform.position.x, -3.9f, 0);
+        }
+        if(transform.position.y>0.0f){
+             transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        if(transform.position.x<-9.22f){
+             transform.position = new Vector3(-9.22f, transform.position.y, 0);
+        }
+        if(transform.position.x>9.22f){
+             transform.position = new Vector3(9.22f, transform.position.y, 0);
+        }
+    }
+
+     public void Damage(){
+        lives --;
+        if(lives < 1)
+        {
+            if(spawnManager_sc != null){
+            spawnManager_sc.OnPlayerDeath();
+            } 
+            Destroy(this.gameObject);
+        }
+    }
+    /*void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            Damage();
+        }
+    }
+    */
+    }
